3cb60f53ba4642c13bcbba94ba228d88206aa5b3
XO <crose11942@gmail.com>
2025-03-11 17:38:44 -0500

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,79 @@
+using System;
+using System.IO;
+using BepInEx;
+using EntityStates;
+using EntityStates.Huntress;
+using EntityStates.Huntress.HuntressWeapon;
+using EntityStates.Huntress.Weapon;
+using R2API;
+using RoR2;
+using UnityEngine;
+
+namespace MAGICALGIRLHUNTRESS;
+
+    [BepInPlugin("com.XO.MagicalGirlHuntress", "MAGICALGIRLHUNTRESS", "1.0.0")]
+    public class MagicalHuntPlugin : BaseUnityPlugin
+    {
+  
+        public static MagicalHuntPlugin instance { get; private set; }
+
+       
+        public void Awake()
+        {
+        LoadAndRegisterAssets();
+        RoR2Application.onLoad += ReplaceVanillaAssets
+
+        }
+
+    private void LoadAndRegisterAssets()
+    {
+        var path = System.IO.Path.GetDirectoryName(this.Info.Location);
+        this.mainBundle = AssetBundle.LoadFromFile(System.IO.Path.Combine(path, MAGICALGIRLHUNTRESS));
+
+        this.OmniImpactVFXHuntress = this.mainBundle.LoadAsset<GameObject>("MAGICALGIRLHUNTRESS");
+        R2API.ContentAddition.AddEffect(this.OmniImpactVFXHuntress);
+
+        this.HuntressFireArrowRain = this.mainBundle.LoadAsset<GameObject>("MAGICALGIRLHUNTRESS");
+        R2API.ContentAddition.AddEffect(this.HuntressFireArrowRain);
+
+        this.TracerHuntressSnipe = this.mainBundle.LoadAsset<GameObject>("MAGICALGIRLHUNTRESS");
+        R2API.ContentAddition.AddEffect(this.TracerHuntressSnipe);
+
+    }
+
+        private void ReplaceVanillaAssets()
+        {
+           EntityState.Huntress.BlinkState.blinkPrefab = this.blinkEffect;
+            EntityState.Huntress.BeginArrowSnipe.blinkPrefab = this.blinkEffect;
+            EntityState.Huntress.GenericBulletBaseState.hitEffectPrefab = this.OmniImpactVFXHuntress;
+            EntityState.Huntress.GenericBulletBaseState.muzzleFlashPrefab = this.HuntressFireArrowRain;
+           EntityState.Huntress.GenericBulletBaseState.tracerEffectPrefab = this.TracerHuntressSnipe;
+ 
+
+        }
+
+
+        public const string PluginGUID = "com.XO.MagicalGirlHuntress";
+
+        public const string PluginAuthor = "XO";
+
+       
+        public const string PluginName = "MagicalGirlHuntress";
+
+       
+        public const string PluginVersion = "1.0.0";
+
+        
+        private AssetBundle mainBundle;
+
+       
+        private const string bundleName = "MAGICALGIRLHUNTRESS";
+
+
+    private GameObject blinkEffect;
+    private GameObject OmniImpactVFXHuntress;
+    private GameObject HuntressFireArrowRain;
+    private GameObject TracerHuntressSnipe;
+    }
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# MagicalGirlHuntress
+# Info
+
+A Magical Girl Themed Huntress skin, including 3 new models with each model have 2 unique color schemes. Inspired by Star Guardians from League of Legends and Sailor Moon! (Does not currently include VFX for the skins, but WIP)
+
+# Screenshots
+
+![](https://i.postimg.cc/Lsn69LCD/Screenshot-261.png)
+![](https://i.postimg.cc/C18hDYVf/Screenshot-262.png)
+
+![](https://i.postimg.cc/T1qT791d/Screenshot-263.png)
+![](https://i.postimg.cc/05p5y8fT/Screenshot-264.png)
+
+![](https://i.postimg.cc/90jWYSWg/Screenshot-265.png)
+![](https://i.postimg.cc/5Ngx8nDp/Screenshot-266.png)
+
+
+
+# Contact
+
+If there's any issues and or you have any ideas on how I can make it better, you can reach me on discord at - kdadrew
+())
+
+# Credits
+
+Made with KingEnderBrine's Skin Builder/tutorial and RuneFox237's tutorials
+
+
+# Changelog
+
+1.0.0 - Initial release
